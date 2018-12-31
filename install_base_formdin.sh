254ce5acd49f609abda45692647a033ad57a05c2
Bjverde <bjverde@yahoo.com.br>
2018-12-31 19:53:41 -0200

    #6 primeira versão

---
+++ install_base_formdin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#STYLE_COLOR
+RED='\033[0;31m';
+LIGHT_GREEN='\e[1;32m';
+NC='\033[0m' # No Color
+
+echo -e "${LIGHT_GREEN} Download FormDin by Git Clone ${NC}"
+cd /var/www/html
+git clone https://github.com/bjverde/formDin.git
+
+echo -e "${LIGHT_GREEN} Baixando SysGen (gerador de sistemas) do github e colocando em www/formdin/sysgen ${NC}"
+cd formDin;
+git clone https://github.com/bjverde/sysgen.git
+
+echo -e "${RED} Criando TMP formDin ${NC}"
+mkdir -p base/tmp/
+chmod 777 base/tmp/
