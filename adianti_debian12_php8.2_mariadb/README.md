2e1aa7c724c7cab471ada34696b4ec52e8022c14
Bjverde <bjverde@yahoo.com.br>
2024-08-31 20:42:28 -0300

    :hammer: #27 primeira versão

---
+++ adianti_debian12_php8.2_mariadb/README.md
@@ -0,0 +1,69 @@
+# adianti-gabarito
+Script Docker / Compose baseada em Debian 12 para desenvolvimento PHP com [Adianti Framework](https://www.adianti.com.br/framework). O container rodará Apache/2.4.29 e PHP 8.2.X, bem como todos os módulos e extensões necessárias para a correta execução do framework. 
+
+Esse ambiente foi configurado conforme o tutorial [Preparando um servidor gabarito para o Adianti Framework (com Ubuntu 22.04 e PHP 8.1)](https://www.adianti.com.br/forum/pt/view_7397?preparando-um-servidor-gabarito-para-o-adianti-framework-com-ubuntu-2204-e-php-81) escrito por [Pablo Dall'Oglio](http://www.dalloglio.net/).
+
+*ESTA CONFIGURADO PARA DESENVOLVIMENTO COM XDebug*
+
+# Detalhando o Ambiente
+
+* Debian 12
+    * locate
+    * mlocate
+    * wget
+    * curl
+    * apt-transport-https
+    * lsb-release
+    * vim 
+    * rpl
+* Apache 2.4.X
+    * AllowOverride All
+    * LDAP Ligado
+* PHP 8.2.X
+    * CURL
+    * JSON
+    * MbString
+    * XML
+    * ZIP
+    * Drive MongoDB
+    * PDO - SqLite
+    * PDO - MySQL
+    * PDO - PostGres
+    * PDO - Sql Server
+        * Drive SqlServer 5.10
+        * ODBC 17
+        * OpenSSL 1.1.1-g 2020
+    * Xdebug 3.X - ATIVADO por padrão
+* Composer
+* PHPUnit 11.X
+* GIT
+
+## Rodando
+Para facilitar a vida foi usado um Docker com Docker-compose basta executar  `docker-compose up --build` e aguardar.
+
+* pasta `www` local do seu projeto. Equivale a pasta `/var/www/html`
+* pasta `log` contem todos os arquivos de log de `/var/log/apache2`
+
+### Alterando o PHP.INI
+Altere o arquivo php.ini na raiz da pasta depois rode novamente `docker-compose up --build` com isso as novas configurações estarão disponiveis. 
+
+Configurações mais comuns
+* `display_errors = On` linha 503 - mostra os erros na tela
+* `error_reporting = E_ALL` linha 486 - mostra todos os tipos de erros
+* `post_max_size = 100M` linha 698 - altera o tamanho maximo do post
+* `upload_max_filesize = 100M` linha 850 - altera o tamanho do upload de arquivo
+
+
+## X-Debug
+Vem ativado por padrão para ativar basta comentar as linhas no arquivo `adianti_debian12_php8.2.Dockerfile` para desativar.
+* 131
+* 134,135 e 136
+* 139
+
+
+# Alterando para modo produção.
+
+1. Alterar arquivo php.ini `display_errors = off` na linha 503 - para não mostrar os errados na tela
+1. Alterar arquivo php.ini `error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT` linha 486 - mostra todos os tipos de erros
+1. Alterar arquivo `adianti_debian12_php8.2.Dockerfile` e comentar as linahs 130 até 139
+1. Alterar arquivo `adianti_debian12_php8.2.Dockerfile` e comentar as linahs 222 para desativar a porta 9003
---
+++ adianti_debian12_php8.2_mariadb/www/check_adianti_server.php
@@ -0,0 +1,178 @@
+<?php
+function phpModulo($extensao=null, $html)
+{
+    if (!extension_loaded($extensao)) {
+        $html->add('<b>'.$extensao.'</b>: <span class="vermelho">Não instalado</span><br>');
+        return true;
+    } else {
+        $html->add('<b>'.$extensao.'</b>: <span class="verde">Instalado</span><br>');
+        return false;
+    }
+}
+function apacheModulo($moduloApache){
+    $listModulos = apache_get_modules();
+    $result = in_array($moduloApache, $listModulos);
+    return $result;
+}
+function extensaoCarregada($extensao=null){
+    if (!extension_loaded($extensao)) {
+        echo '<li>'.$extensao.'</b>: <span class="vermelho">Não instalada</span></li>';
+    } else {
+        echo '<li>'.$extensao.'</b>: <span class="verde">Instalada.</span></li>';
+    }
+}
+function info(){
+	echo '<div id="instrucoes" class="main">';
+	echo 'Versão Atual do PHP: ' . phpversion();
+	echo '<br> Sistema Operacional: ' . PHP_OS;
+	echo '</div>';
+}
+function listaExtensoesBanco(){
+	echo '<h3><a href="javascript:toggleDiv(\'listaExtensoesBanco\');">Banco de dados</a></h3>';
+	echo '<div id="listaExtensoesBanco" class="alert-box warning">';
+	echo '<ul>';
+	extensaoCarregada('pdo');
+	extensaoCarregada('pdo_sqlite');
+	extensaoCarregada('sqlite3');
+	extensaoCarregada('pdo_sqlsrv');
+    extensaoCarregada('sqlsrv');
+    extensaoCarregada('pdo_mysql');
+    extensaoCarregada('PDO_OCI');
+    extensaoCarregada('ODBC');
+    extensaoCarregada('pdo_pgsql');
+    extensaoCarregada('pdo_sqlite');
+	echo '</ul>';
+	echo '</div>';
+}
+function listaExtensoesGerais(){
+	echo '<h3><a href="javascript:toggleDiv(\'listaExtensoesGerais\');">Gerais</a></h3>';
+	echo '<div id="listaExtensoesGerais" class="alert-box warning">';
+	echo '<ul>';
+    extensaoCarregada('curl');
+	extensaoCarregada('gd');
+    extensaoCarregada('json');
+    extensaoCarregada('ldap');
+	extensaoCarregada('mbstring');
+    extensaoCarregada('SimpleXML');
+	extensaoCarregada('soap');
+	extensaoCarregada('xml');
+	extensaoCarregada('xsl');
+    extensaoCarregada('zip');
+	echo '</ul>';
+	echo '</div>';
+}
+function listaExtensoes(){
+	echo '<hr>';
+	echo '<h2>Extensões PHP</h2>';
+	listaExtensoesBanco();
+	listaExtensoesGerais();
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<!--
+    <Exploding edict - This software is used to format string>
+    Copyright (C) 2014  - Reinaldo A. Barrêto Jr - bjverde at yahoo.com.br
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
+<head>
+<meta charset="UTF-8"/>
+<title>Check Adianti Server</title>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<style type="text/css">
+    body{
+		font-size: 15px;
+		font-family: Verdana,Geneva,Tahoma,Arial,Helvetica,sans-serif;
+    }
+	.main {
+		font-size: 15px;
+		font-family: Verdana,Geneva,Tahoma,Arial,Helvetica,sans-serif;
+	}
+	#instrucoes{
+		padding: 0.2%;
+		width: auto;
+		background-color: #F1F1F1;
+		border-radius: 4px;
+		border: 1px solid #EDEDED;
+	}
+	#exemplo{
+		display: none;
+		padding: 0.2%;
+		width: auto;
+		background-color: #E0F0BA;
+		border-radius: 4px;
+		border: 1px solid #EDEDED;		
+	}
+	
+	#changelog{
+		display: none;		
+	}
+	.alert-box {
+		color:#555;
+		border-radius:10px;
+		font-family:Tahoma,Geneva,Arial,sans-serif;
+		padding:10px 10px 10px 36px;
+		margin:10px;
+	}
+	.warning {
+		background:#fff8c4 no-repeat 10px 50%;
+		border:1px solid #f2c779;
+	}
+	.success {
+		background:#e9ffd9 no-repeat 10px 50%;
+		border:1px solid #a6ca8a;
+	}
+	.notice {
+		background:#e3f7fc no-repeat 10px 50%;
+		border:1px solid #8ed9f6;
+	}
+    .vermelho {
+        color:#ff0000;
+        font-weight:bold;
+    }
+    .verde {
+        color:#008000;
+        font-weight:bold;
+    }
+    .versao {
+        color:#0000FF;
+        font-weight:bold;
+        font-size:16px;
+    }
+    ul {
+        -webkit-column-count: 3;
+        -moz-column-count: 3;
+        column-count: 3;
+    }
+</style>
+<script>
+	function toggleDiv(divId) {
+	   $("#"+divId).toggle();
+	}
+</script>
+</head>
+<body>
+<h1>CheckList para Adianti FrameWork ou Adianti Template</h1>
+</br>
+</br>Baseado no artigo <a href="https://www.adianti.com.br/forum/pt/view_7397?preparando-um-servidor-gabarito-para-o-adianti-framework-com-ubuntu-2204-e-php-81" target="_blank">Preparando um servidor gabarito para o Adianti Framework (com Ubuntu 22.04 e PHP 8.1)</a> do Pablo Dall'Oglio
+</br>
+</br>
+<?php
+    info();
+    listaExtensoes();
+?>
+</html>
